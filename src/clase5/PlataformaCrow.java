1381a5a7322aa08f92c60dd9c99f63e1949a40a4
leuaxe <leuaxe-488@hotmail.com>
2019-11-02 14:01:12 -0500

    metodos set/get y metodos necesarios en la clase

--- src/clase5/PlataformaCrow.java
+++ src/clase5/PlataformaCrow.java
@@ -1,5 +1,41 @@
 package clase5;
 
 public class PlataformaCrow{
-    
+    private String nombre;
+    private double dineroPropio;
+    private static double dineroTotal = 0.0;
+
+    public String getNombre(){
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public double getDineroPropio(){
+        return this.dineroPropio;
+    }
+
+    public void setDineroPropio(double dineroPropio){
+        this.dineroPropio = dineroPropio;
+    }
+
+    public static double getDineroTotal(){
+        return dineroTotal;
+    }
+
+    public static void setDineroTotal(double dineroTotal){
+        PlataformaCrow.dineroTotal = dineroTotal;
+    }
+
+    public void asignarDinero(double cantidadDinero){
+        dineroTotal -= cantidadDinero;
+        this.dineroPropio = cantidadDinero;
+    }
+
+    //como es un metodo comun para todas las plataformas se pone en static
+    public static void imprimirDineroTotal(){
+        System.out.println("El dinero total del que se dispone es "+ dineroTotal);
+    }
 }
