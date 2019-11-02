fe31afb1e03b51d1864b530ffc9b807b06ccce92
leuaxe <leuaxe-488@hotmail.com>
2019-11-02 17:35:53 -0500

    se crea 3 objetos y se llama al metodo estatico de Usuario

--- src/clase5/Plataforma.java
+++ src/clase5/Plataforma.java
@@ -3,5 +3,9 @@ package clase5;
 public class Plataforma{
     public static void main(String[] args) {
         Usuario u1 = new Usuario("Juan", 300.0);
+        Usuario u2 = new Usuario("Antonio", 150.0);
+        Usuario u3 = new Usuario("Pepe", 500.0);
+
+        Usuario.imprimirNumUsuarios();
     }
 }
